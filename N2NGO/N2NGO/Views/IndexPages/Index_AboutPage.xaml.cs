7e1b8654e5dde88ca287f40451c5bdda7b0bb61f
LGF <3239215285@qq.com>
2024-10-15 23:50:10 +0800

    Update About Index Page

--- N2NGO/N2NGO/Views/IndexPages/Index_AboutPage.xaml.cs
+++ N2NGO/N2NGO/Views/IndexPages/Index_AboutPage.xaml.cs
@@ -24,34 +24,31 @@ namespace N2NGO.Views.SubPages
             {
                 IndexerTitle = "N2N GO",
                 IndexerDescription = string.Format("版本 {0}\n构建日期 {1}\n链接 {2}\nRuntime {3}\n.NET {4}",
-                SharedData.VersionString,
-                "8/28/2024",
-                "https://gitee.com/xue-jiangbin/n2nmc-private",
-                (Environment.Is64BitProcess ? "64-bit" : "32-bit"),
-                RuntimeEnvironment.GetRuntimeDirectory()
-                ),
+                    SharedData.VersionString,
+                    "8/28/2024",
+                    "https://gitee.com/xue-jiangbin/n2nmc-private",
+                    (Environment.Is64BitProcess ? "64-bit" : "32-bit"),
+                    RuntimeEnvironment.GetRuntimeDirectory()),
                 NagivKey = "N2NGO",
-                IndexerCBI = new BitmapImage(new("/Data/Images/N2N GO Preview.png", UriKind.Relative))
+                IndexerCBI = new BitmapImage(new("/Data/Images/N2N GO.png", UriKind.Relative))
             });
 
             Indexers.Items.Add(new Indexer
             {
                 IndexerTitle = "N2N GO Core",
                 IndexerDescription = string.Format("版本 {0}\n构建日期 {1}\n链接 {2}\nRuntime {3}\n.NET {4}",
-                SharedData.Version,
-                "8/28/2024",
-                "https://gitee.com/xue-jiangbin/n2nmc-private",
-                (Environment.Is64BitProcess ? "64-bit" : "32-bit"),
-                RuntimeEnvironment.GetRuntimeDirectory()
-                )
+                    SharedData.Version,
+                    "8/28/2024",
+                    "https://gitee.com/xue-jiangbin/n2nmc-private",
+                    (Environment.Is64BitProcess ? "64-bit" : "32-bit"),
+                    RuntimeEnvironment.GetRuntimeDirectory())
             });
 
             Indexers.Items.Add(new Indexer
             {
                 IndexerTitle = "edge - n2n",
                 IndexerDescription = string.Format("版本 {0}\n构建日期 {1}\n链接 {2}",
-                    "v.3.1.1", "8/2/2024", "https://github.com/control0forver/n2npp"
-                    )
+                    "v.3.1.1", "8/2/2024", "https://github.com/control0forver/n2npp")
             });
 
             Indexers.Items.Add(new Indexer
@@ -60,7 +57,7 @@ namespace N2NGO.Views.SubPages
                 IndexerDescription =
                 "HandyControl (3.5.1)\n" +
                 "MagicEffects (1.0.0)\n" +
-                "SharpVectors (1.8.4)\n" +
+                "SharpVectors (1.8.4.2)\n" +
                 "Walterlv.Themes.FluentDesign (7.9.0)"
             });
         }
